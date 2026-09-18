41ed002abcc0f4b765c18a8e82f6e3e2ef7e2881
skynetfrank <frank.uah@gmail.com>
2026-09-18 09:11:56 -0400

    starting router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,40 +1,25 @@
-import { useState } from 'react'
-import heroImg from './assets/hero.png'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import './App.css'
+import { useState, useEffect, useRef } from "react";
+import { Link, Outlet } from "react-router";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Frontend Standard</h1>
-         
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
-
-      <div className="ticks"></div>
-
-    
-
-     
+      <div className="grid-container">
+        <header className="app-header">
+          <div className="header-actions"></div>
+        </header>
+        <main>
+          <Outlet />
+        </main>
+        <footer>
+          <div className="footer-content">
+            <div className="social-icons"></div>
+          </div>
+        </footer>
+      </div>
+      {/* El modal se renderiza aquí para estar disponible en toda la app */}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router";
+import "./index.css";
+import App from "./App";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const router = createBrowserRouter(createRoutesFromElements(<Route path="/" element={<App />}></Route>));
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  //<Provider store={store}>
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+  // </Provider>,
+);
